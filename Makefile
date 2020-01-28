36e4fd8d3cbc4179246831d5faf52047b1d9ef7b
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-01-28 10:13:33 +0100

    confed

--- Makefile
+++ Makefile
@@ -1,4 +1,6 @@
 VERSION=$(shell git describe --tags)
 
-install: bao.sh
-	install -m +x -v bao.sh $$HOME/.local/bin/bao
+install: bao.sh bao.yaml
+	install -m 755 -v bao.sh $$HOME/.local/bin/bao
+	install -v -d $$HOME/.local/etc/bao/
+	install -m 400 -v bao.yaml $$HOME/.local/etc/bao/bao.yaml
--- bao.sh
+++ bao.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+action=${1:-login}
 
-if [ "${1:-ssh}" ==  "mount" ];  then
-    sshfs baobab2.unige.ch:/ bao -o ssh_command='sshpass -f '<(keyring get unige savchenk)' ssh';
-else
+shift
+args=$@
+
+function get-config() {
+    path=${1:?}
+    v=$(< $HOME/.local/etc/bao/bao.yaml yq -r $path)
+    eval echo $v
+}
+
+
+
+function mount() {
+    mountpoint=$(get-config .sshfs_mountpoint)
+    echo "mounting as $mountpoint"
+    fusermount -u $mountpoint || echo 'can not unmount'
+    mkdir -pv $mountpoint
+    sshfs baobab2.unige.ch:/ $mountpoint -o ssh_command='sshpass -f '<(keyring get unige savchenk)' ssh';
+}
+
+function login() {
     sshpass -f <(keyring get unige savchenk) ssh -Y baobab2.unige.ch $@;
-fi
+}
+
+$action $args
