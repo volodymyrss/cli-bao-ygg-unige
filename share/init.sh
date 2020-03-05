a05a3e0e8a59802b1685bbd0a76c92740180cd39
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-03-05 16:55:48 +0100

    sing repo in sctatch

--- share/init.sh
+++ share/init.sh
@@ -3,7 +3,8 @@ module load GCCcore/8.2.0 Singularity/3.4.0-Go-1.12
 export REP_BASE_PROD=$HOME/scratch/data/integral
 export REP_BASE_PROD_NRT=$HOME/scratch/data/integral-nrt
 
-export SINGULARITY_LOCAL_REPO=$HOME/scratch/singularity/
+export SINGULARITY_LOCAL_REPO=/srv/beegfs/scratch/users/s/savchenk/singularity/
+#export SINGULARITY_LOCAL_REPO=$HOME/scratch/singularity/
 export DATA_ROOT=/srv/beegfs/scratch/users/s/savchenk/data/
 
 export DDCACHE_ROOT=$DATA_ROOT/reduced/ddcache
